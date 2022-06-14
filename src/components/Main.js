047600561debf3a3d74a07870083c875c70aba0b
AnthonyHarold67 <anthonyharold67@gmail.com>
2022-06-14 21:41:08 +0300

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Main = ({id,name,image,price,symbol,marketcap,pricechange}) => {
+
+  return (
+    <div className='coin-container'>
+      <div className="coin-row">
+      <div className="coin">
+      <img src={image} alt={name} />
+      <h1>{name}</h1>
+      </div>
+      <div className="coin-data">
+        <p className="coin-price">{price.toLocaleString()}$</p>
+        {
+          pricechange < 0 ? (
+            <p className="coin-percent red">{pricechange.toFixed(2)}%</p>
+          ):(
+            <p className="coin-percent green">{pricechange.toFixed(2)}</p>
+          )
+        }
+        <p className="coin-marketcap">Mkt.Cap.Rs {marketcap.toLocaleString()}</p>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
